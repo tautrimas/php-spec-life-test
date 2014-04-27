7cb5e40a7dacc62f5b19455785fdf497ff7ddf9b
Tautrimas Pajarskas <tautrimas@tautrimas.lt>
2014-04-27 19:34:03 +0300

    Neigh counting

--- spec/Life/MatrixSpec.php
+++ spec/Life/MatrixSpec.php
@@ -11,4 +11,17 @@ class MatrixSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Life\Matrix');
     }
+
+    function it_tells_new_cells_state()
+    {
+        $this->calculateNewState(1, 0)->shouldReturn(0);
+        $this->calculateNewState(1, 1)->shouldReturn(0);
+        $this->calculateNewState(1, 2)->shouldReturn(1);
+        $this->calculateNewState(1, 3)->shouldReturn(1);
+        $this->calculateNewState(1, 4)->shouldReturn(0);
+        // 5..7
+        $this->calculateNewState(1, 8)->shouldReturn(0);
+
+        $this->calculateNewState(0, 3)->shouldReturn(1);
+    }
 }
--- src/Life/Matrix.php
+++ src/Life/Matrix.php
@@ -4,4 +4,20 @@ namespace Life;
 
 class Matrix
 {
+    public function calculateNewState($currentState, $count)
+    {
+        if ($currentState === 0 && $count === 3) {
+            return 1;
+        }
+
+        if ($currentState === 1) {
+            if ($count < 2 || $count > 3) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
 }
